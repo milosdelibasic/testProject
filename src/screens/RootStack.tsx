ff61fdd0b61e0038a2c8bacff15d838664b6e2aa
MilosDelibasicAlmost <milos@almostapps.com>
2025-03-01 15:16:43 +0100

    screens utils & implement them in RootStack

--- src/screens/RootStack.tsx
+++ src/screens/RootStack.tsx
@@ -6,6 +6,8 @@ import LoginScreen from './LoginScreen';
 import RegisterScreen from './RegisterScreen';
 import ProfileScreen from './ProfileScreen';
 
+import { screens } from '../utils/screens';
+
 const Stack = createNativeStackNavigator();
 
 const RootStack: React.FC = () => {
@@ -14,17 +16,21 @@ const RootStack: React.FC = () => {
   if (isLoggedIn) {
     return (
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name={screens.homeScreen} component={HomeScreen} />
+        <Stack.Screen name={screens.profileScreen} component={ProfileScreen} />
       </Stack.Navigator>
     );
   }
 
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Auth" component={AuthScreen} />
-      <Stack.Screen name="Login" component={LoginScreen} />
-      <Stack.Screen name="Register" component={RegisterScreen} />
+      <Stack.Screen
+        name={screens.authScreen}
+        component={AuthScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen name={screens.loginScreen} component={LoginScreen} />
+      <Stack.Screen name={screens.registerScreen} component={RegisterScreen} />
     </Stack.Navigator>
   );
 };
---
+++ src/utils/screens.ts
@@ -0,0 +1,7 @@
+export const screens = {
+  authScreen: 'Auth',
+  loginScreen: 'Login',
+  registerScreen: 'Register',
+  homeScreen: 'Home',
+  profileScreen: 'Profile'
+};
