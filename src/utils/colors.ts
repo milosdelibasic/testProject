b9ee5ea8aaa37795ed0f71dd80b1f675057f7535
MilosDelibasicAlmost <milos@almostapps.com>
2025-03-01 15:09:36 +0100

    basic colors

---
+++ src/utils/colors.ts
@@ -0,0 +1,26 @@
+export const colors = {
+  // Backgrounds
+  primaryBackground: '#0A0D1F',
+  secondaryBackground: '#1B2635',
+  cardBackground: '#16222E',
+
+  // Text
+  primaryText: '#EAEAEA',
+  secondaryText: '#A1A1A1',
+  headingText: '#FFFFFF',
+  textOnDark: '#D1D1D1',
+
+  // Buttons
+  primaryButtonBackground: '#3A6CA9',
+  secondaryButtonBackground: '#556A88',
+  buttonText: '#FFFFFF',
+
+  // Accents/Highlights
+  accentColor: '#58A6FF',
+  hoverButtonColor: '#4A7B98',
+  borderColor: '#3D4952',
+
+  // Error/Warning
+  errorColor: '#FF4F5A',
+  warningColor: '#FFCC00'
+};
