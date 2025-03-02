02b14d0c29dba16fe00eed164de41efdce983030
MilosDelibasicAlmost <milos@almostapps.com>
2025-03-02 13:15:22 +0100

    Profile screen UI + fetch user info

--- src/redux/slices/authSlice.ts
+++ src/redux/slices/authSlice.ts
@@ -1,12 +1,15 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
+import { User } from '../../utils/interfaces';
+
 const BASE_URL = 'https://reqres.in/api';
 
 interface AuthState {
-  user: { email: string } | null;
+  user: User | null;
   token: string | null;
   loading: boolean;
+  loadingUserInfo: boolean;
   error: string | null;
 }
 
@@ -14,21 +17,46 @@ const initialState: AuthState = {
   user: null,
   token: null,
   loading: false,
+  loadingUserInfo: false,
   error: null
 };
+
+export const fetchUserProfile = createAsyncThunk(
+  'auth/fetchUserProfile',
+  async (id: number, { rejectWithValue }) => {
+    try {
+      const response = await axios.get(`${BASE_URL}/users/${id}`);
+      return response.data.data;
+    } catch (error: any) {
+      return rejectWithValue('Failed to fetch user info');
+    }
+  }
+);
 
 export const loginUser = createAsyncThunk(
   'auth/login',
   async (
     { email, password }: { email: string; password: string },
-    { rejectWithValue }
+    { dispatch, rejectWithValue }
   ) => {
     try {
       const response = await axios.post(`${BASE_URL}/login`, {
         email,
         password
       });
-      return { email, token: response.data.token };
+      const token = response.data.token;
+
+      // Fetch all users to find the logged user's ID since their API doesn't return user id upon login, just token
+      const usersResponse = await axios.get(`${BASE_URL}/users`);
+      const user = usersResponse.data.data.find((u: User) => u.email === email);
+
+      if (!user) {
+        return rejectWithValue('User not found');
+      }
+
+      dispatch(fetchUserProfile(user.id));
+
+      return { email, token, id: user.id };
     } catch (error: any) {
       return rejectWithValue(error.response?.data?.error || 'Login failed');
     }
@@ -39,14 +67,21 @@ export const registerUser = createAsyncThunk(
   'auth/register',
   async (
     { email, password }: { email: string; password: string },
-    { rejectWithValue }
+    { dispatch, rejectWithValue }
   ) => {
     try {
       const response = await axios.post(`${BASE_URL}/register`, {
         email,
         password
       });
-      return { token: response.data.token, email };
+
+      //Upon registration, we do get user id so we can immediately fetch user info
+      const id = response.data.id;
+      const token = response.data.token;
+
+      dispatch(fetchUserProfile(id));
+
+      return { email, token, id };
     } catch (error: any) {
       return rejectWithValue(
         error.response?.data?.error || 'Registration failed'
@@ -68,6 +103,7 @@ const authSlice = createSlice({
   },
   extraReducers: builder => {
     builder
+      //Login
       .addCase(loginUser.pending, state => {
         state.loading = true;
         state.error = null;
@@ -81,18 +117,32 @@ const authSlice = createSlice({
         state.loading = false;
         state.error = action.payload as string;
       })
+      //Registration
       .addCase(registerUser.pending, state => {
         state.loading = true;
         state.error = null;
       })
       .addCase(registerUser.fulfilled, (state, action) => {
         state.loading = false;
-        state.user = { email: action.payload.email };
+        state.user = { email: action.payload.email, id: action.payload.id };
         state.token = action.payload.token;
       })
       .addCase(registerUser.rejected, (state, action) => {
         state.loading = false;
         state.error = action.payload as string;
+      })
+      //Fetch User Profile
+      .addCase(fetchUserProfile.pending, state => {
+        state.loadingUserInfo = true;
+      })
+      .addCase(fetchUserProfile.fulfilled, (state, action) => {
+        state.loadingUserInfo = false;
+        if (state.user) {
+          state.user = { ...state.user, ...action.payload };
+        }
+      })
+      .addCase(fetchUserProfile.rejected, state => {
+        state.loadingUserInfo = false;
       });
   }
 });
--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,11 +1,104 @@
-import { Text, View } from 'react-native';
+import { ActivityIndicator, Image, StyleSheet, Text, View } from 'react-native';
+
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { useNavigation } from '@react-navigation/native';
+import { useDispatch, useSelector } from 'react-redux';
+
+import Button from '../components/Button';
+
+import { RootStackParamList, screens } from '../utils/screens';
+import { AppDispatch, RootState } from '../redux/store';
+import { logout } from '../redux/slices/authSlice';
+
+import { colors } from '../utils/colors';
 
 const ProfileScreen: React.FC = () => {
+  const { user, loadingUserInfo } = useSelector(
+    (state: RootState) => state.auth
+  );
+
+  const navigation =
+    useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+  const dispatch = useDispatch<AppDispatch>();
+
+  const navigateToEditProfile = () => {
+    navigation.navigate(screens.editProfileScreen);
+  };
+
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <SafeAreaView edges={['bottom']} style={styles.container}>
+      {loadingUserInfo ? (
+        <ActivityIndicator size="large" color="white" />
+      ) : (
+        <View style={styles.profileContainer}>
+          <Image source={{ uri: user?.avatar }} style={styles.profileImage} />
+          <Text style={styles.name}>
+            {user?.first_name} {user?.last_name}
+          </Text>
+          <Text style={styles.email}>{user?.email}</Text>
+          {user?.id && <Text style={styles.userId}>ID: {user.id}</Text>}
+        </View>
+      )}
+
+      <View style={styles.bottom}>
+        <Button
+          onPress={navigateToEditProfile}
+          style="primary"
+          label="Edit Profile"
+        />
+        <Button onPress={handleLogout} style="secondary" label="Sign out" />
+        <Button onPress={() => {}} style="tertiary" label="Delete account" />
+      </View>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.primaryBackground,
+    paddingHorizontal: 16,
+    paddingVertical: 16,
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  profileContainer: {
+    alignItems: 'center',
+    gap: 6
+  },
+  profileImage: {
+    width: 120,
+    height: 120,
+    borderRadius: 60,
+    backgroundColor: colors.accentColor,
+    borderColor: colors.accentColor,
+    borderWidth: 3,
+    marginBottom: 10
+  },
+  name: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: colors.primaryText
+  },
+  email: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: colors.textOnDark
+  },
+  userId: {
+    fontSize: 16,
+    color: colors.secondaryText,
+    marginTop: 4
+  },
+  bottom: {
+    width: '100%',
+    gap: 20
+  }
+});
 
 export default ProfileScreen;
